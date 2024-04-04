d1cbcd30f42c2f6b5279921049f628f4021563ef
mainak <heellomeelo@gmail.com>
2024-04-04 18:49:20 +0530

    adding task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type Priority = 'p1' | 'p2' | 'p3';
 
 type Task = {
@@ -8,18 +10,38 @@ type Task = {
 };
 
 function App() {
-  const tasks: Task[] = [
+  const [taskName, setTaskName] = useState('');
+
+  const [tasks, setTasks] = useState<Task[]>([
     {
       id: 1,
       title: 'Learn React',
       isCompleted: true,
       priority: 'p1',
     },
-  ];
+  ]);
+
+  const onAddTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: new Date().getTime(), // Not a great way to generate IDs
+        title: taskName,
+        isCompleted: false,
+      },
+    ]);
+  };
 
   return (
     <div>
       <h1>Tasks</h1>
+      <label htmlFor="task-input">Add Task: </label>
+      <input
+        id="task-input"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+      <button onClick={onAddTask}>Add</button>
       <ul>
         {tasks.map((task) => (
           <li key={task.id}>{task.title}</li>
